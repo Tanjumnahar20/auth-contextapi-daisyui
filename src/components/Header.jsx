65446547de85a6a1ca7f5846ebfb433575195e9b
Tanjum Nahar <tanjumnaharchandana@gmail.com>
2024-01-23 10:31:21 +0600

    auth observer added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+
+     const {user} = useContext(AuthContext);
+
     return (
         <div>
    
@@ -11,6 +15,17 @@ const Header = () => {
   <Link className="btn btn-ghost text-xl" to='/'>Home</Link>
   <Link className="btn btn-ghost text-xl" to='/login'>Login</Link>
   <Link className="btn btn-ghost text-xl" to='/register'>Register</Link>
+
+  {
+    user ? <>
+    <span>{user.email}</span>
+    
+   <button className="btn btn-xs">sign out</button>
+
+    </>:
+    <Link to='/login'>login</Link>
+
+  }
 </div>
     
 </div>
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,35 +1,48 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../firebase/firebase.init';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 
- export const AuthContext =createContext(null);
+export const AuthContext = createContext(null);
 
 
 
- const auth = getAuth(app);
+const auth = getAuth(app);
 
-const AuthProviders = ({children}) => {
-    const [user,setUser] = useState(null);
+const AuthProviders = ({ children }) => {
+    const [user, setUser] = useState(null);
 
-    const createUser = (email,password)=>{
-        return createUserWithEmailAndPassword(auth,email,password)
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const signIn = (email,password) =>{
-        return signInWithEmailAndPassword(auth,email,password)
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const authInfo ={
+    const authInfo = {
         user,
         createUser,
         signIn
     }
+
+    //   on auth state change
+
+    useEffect(() => {
+     const unsubscribe =onAuthStateChanged(auth,currentUser =>{
+        console.log(currentUser);
+        setUser(currentUser)
+     })
+     return ()=>{
+        unsubscribe();
+     }
+    }, [])
+
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
-             {children}
+                {children}
             </AuthContext.Provider>
         </div>
     );
